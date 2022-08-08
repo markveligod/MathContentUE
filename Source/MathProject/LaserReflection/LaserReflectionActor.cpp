b156591194adf7aa83942328abb1dcd11f4e5203
Mark <34418187+markveligod@users.noreply.github.com>
2022-08-08 09:31:20 +0500

    Complete Laser Reflection Case

---
+++ Source/MathProject/LaserReflection/LaserReflectionActor.cpp
@@ -0,0 +1,114 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LaserReflection/LaserReflectionActor.h"
+
+#include "Librarys/MathProjectLibrary.h"
+
+#pragma region Default
+
+// Sets default values
+ALaserReflectionActor::ALaserReflectionActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(FName("Root scene component"));
+	SetRootComponent(SceneRoot);
+
+	RayMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("Ray mesh"));
+	RayMesh->SetupAttachment(SceneRoot);
+
+	ReflectionMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("Reflection mesh"));
+	ReflectionMesh->SetupAttachment(SceneRoot);
+}
+
+// Called when the game starts or when spawned
+void ALaserReflectionActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FOnTimelineFloat MoveRayMeshTimelineCallback;
+	MoveRayMeshTimelineCallback.BindDynamic(this, &ALaserReflectionActor::UpdateMoveRayMeshTimeline);
+	MoveRayMesh.AddInterpFloat(MoveRayMeshCurve, MoveRayMeshTimelineCallback);
+	MoveRayMesh.SetLooping(true);
+	MoveRayMesh.Play();
+
+	FOnTimelineFloat MoveReflectionMeshTimelineCallback;
+	MoveReflectionMeshTimelineCallback.BindDynamic(this, &ALaserReflectionActor::UpdateMoveReflectionMeshTimeline);
+	MoveReflectionMesh.AddInterpFloat(MoveReflectionMeshCurve, MoveReflectionMeshTimelineCallback);
+	MoveReflectionMesh.SetLooping(true);
+	MoveReflectionMesh.Play();
+}
+
+void ALaserReflectionActor::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	FlushPersistentDebugLines(UMathProjectLibrary::GetWorldInEditor());
+	UMathProjectLibrary::RestartPushDebugLine(UMathProjectLibrary::GetWorldInEditor());
+}
+
+void ALaserReflectionActor::OnFlushPersistentDebug_Implementation(UWorld* World)
+{
+	UpdateDraw(World, true);
+}
+
+#if WITH_EDITOR
+
+void ALaserReflectionActor::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+	if (!PropertyChangedEvent.Property) return;
+	
+	FlushPersistentDebugLines(UMathProjectLibrary::GetWorldInEditor());
+	UMathProjectLibrary::RestartPushDebugLine(UMathProjectLibrary::GetWorldInEditor());
+}
+
+#endif
+
+// Called every frame
+void ALaserReflectionActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	MoveRayMesh.TickTimeline(DeltaTime);
+	MoveReflectionMesh.TickTimeline(DeltaTime);
+	UpdateDraw(GetWorld(), false);
+}
+
+void ALaserReflectionActor::UpdateDraw(const UWorld* World, bool bPersistentLines)
+{
+	FVector L_RelPosRefMesh = ReflectionMesh->GetRelativeLocation();
+	L_RelPosRefMesh.Y = RelPosReflectionMesh + DelayPosReflectionMesh;
+	ReflectionMesh->SetRelativeLocation(L_RelPosRefMesh);
+
+	FHitResult HitResult;
+	const FVector& RayStart = RayMesh->GetComponentLocation();
+	DirectionRay.Y = DelayPosRayMesh;
+	FVector RayEnd = RayStart + DirectionRay;
+
+	World->LineTraceSingleByChannel(HitResult, RayStart, RayEnd, ECollisionChannel::ECC_Visibility);
+	if (HitResult.bBlockingHit)
+	{
+		RayEnd = HitResult.Location;
+		DrawDebugDirectionalArrow(World, RayEnd, RayEnd + HitResult.Normal * 60.0f, 3.0f, FColor::Blue, bPersistentLines, 0.0f, 0, 2.0f);
+		FVector L_DirRay = (RayEnd - RayStart).GetSafeNormal();
+		FVector DirectionReflection = L_DirRay - 2 * FVector::DotProduct(L_DirRay, HitResult.Normal) * HitResult.Normal;
+		DrawDebugLine(World, RayEnd, RayEnd + DirectionReflection * 60.0f, FColor::Red, bPersistentLines, 0.0f, 0, 2.0f);
+	}
+	DrawDebugLine(World, RayStart, RayEnd, FColor::Red, bPersistentLines, 0.0f, 0, 2.0f);
+	
+}
+
+void ALaserReflectionActor::UpdateMoveRayMeshTimeline(float NewValue)
+{
+	DelayPosRayMesh = NewValue;
+}
+
+void ALaserReflectionActor::UpdateMoveReflectionMeshTimeline(float NewValue)
+{
+	DelayPosReflectionMesh = NewValue;
+}
+
+#pragma endregion
---
+++ Source/MathProject/LaserReflection/LaserReflectionActor.h
@@ -0,0 +1,85 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/TimelineComponent.h"
+#include "GameFramework/Actor.h"
+#include "Librarys/FlushDebugInterface.h"
+#include "LaserReflectionActor.generated.h"
+
+UCLASS()
+class MATHPROJECT_API ALaserReflectionActor : public AActor, public IFlushDebugInterface
+{
+	GENERATED_BODY()
+
+#pragma region Default
+	
+public:	
+	// Sets default values for this actor's properties
+	ALaserReflectionActor();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	virtual void OnConstruction(const FTransform& Transform) override;
+	virtual void OnFlushPersistentDebug_Implementation(UWorld* World) override;
+
+#if WITH_EDITOR
+
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+
+#endif
+
+private:
+	void UpdateDraw(const UWorld* World, bool bPersistentLines);
+
+#pragma endregion
+
+#pragma region Components
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Components")
+	USceneComponent* SceneRoot{nullptr};
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* RayMesh{nullptr};
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* ReflectionMesh{nullptr};
+
+#pragma endregion
+
+#pragma region Data
+
+protected:
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Laser Reflection Data")
+	FVector DirectionRay{-350.0f, 0.0f, 0.0f};
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Laser Reflection Data", meta = (ClampMin = "-150.0", ClampMax = "150.0"))
+	float RelPosReflectionMesh{0.0f};
+
+	UPROPERTY(EditAnywhere, Category = "Laser Reflection Data")
+	UCurveFloat* MoveRayMeshCurve{nullptr};
+	UPROPERTY(EditAnywhere, Category = "Laser Reflection Data")
+	UCurveFloat* MoveReflectionMeshCurve{nullptr};
+
+	FTimeline MoveRayMesh;
+	FTimeline MoveReflectionMesh;
+
+	float DelayPosRayMesh{0.0f};
+	float DelayPosReflectionMesh{0.0f};
+
+	UFUNCTION()
+	void UpdateMoveRayMeshTimeline(float NewValue);
+	UFUNCTION()
+	void UpdateMoveReflectionMeshTimeline(float NewValue);
+
+#pragma endregion
+};
+
